82410a0d75d8be78edab37939ababd64fbfaa3ef
Nikko Calumpiano <fidelbramosjunior@gmail.com>
2026-05-19 10:03:27 +0800

    Queue attendance emails and add jobs table migration

--- app/Mail/StudentAttendanceNotification.php
+++ app/Mail/StudentAttendanceNotification.php
@@ -5,12 +5,13 @@ namespace App\Mail;
 use App\Models\Attendance;
 use App\Models\Student;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class StudentAttendanceNotification extends Mailable
+class StudentAttendanceNotification extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
